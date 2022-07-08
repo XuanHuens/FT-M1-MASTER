e788c29c3f2e01fa05108fdfc4112a965aff6b5d
Xuan Huens <xuanja1cat@gmail.com>
2022-07-08 01:45:55 -0300

    homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,6 +2,14 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  var suma = 0
+  var posicion = 0
+  for (let i = num.length - 1; i >= 0; i--) {
+    suma = suma + num[i] *2 ** posicion
+    posicion++
+    
+  }
+  return suma
 
 }
 
